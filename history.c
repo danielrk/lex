351870aede8d6bd18efb9516c10dc2d48f2f7e43
danielrk <danielrheekim@gmail.com>
2014-10-05 18:42:25 -0400

    hExpand partially written

--- history.c
+++ history.c
@@ -1,11 +1,100 @@
+// #include <string.h>  // included in mainLex.c
 // #include <stdlib.h>  // getLine.c includes this
 #include "Histlist.h"
 
 static int histsize = 0;
 static Histlist hist = createH();
 
+// Return recursively-expanded string
 char *hExpand (const char *oldLine, int *status)
 {
+    int exclam = strspn(oldLine, "!"); // null chars can't be put in stdin
+
+    char *prefix; // oldLine before first '!'; may be empty/entire string
+    char *sub;    // hist substitution if requested, otherwise "!"
+    char *suffix; // substring of oldLine beyond substring to be 
+                  //   substituted, if any; otherwise oldLine[exclam+1 to end]
+                  //   can be empty string
+
+
+    // PREFIX same in all cases
+    prefix = malloc(sizeof(char) * (exclam + 1));
+    strncpy (prefix, oldLine, exclam);
+    prefix[exclam] = '\0';
+    
+
+    // Decode event
+    int first_stat = get_sub_suf (oldLine+exclam, &sub, &suffix);
+
+    if (oldLine[exclam+1] == '!') { // "!!"
+        return three_cat (prefix, sub, );
+    }
+    else if () { // "!N"
+    }
+    else if () { // "!-N"
+    }
+    else if () { // "!?STRING?"
+    }
+    else { // "![x]*"; SUB = "!"
+    }
+    free(prefix);
+    free(sub);
+    free(suffix);
+}
+
+// EXSTR is a string of the form "![...]", which begins with a possible
+// substitution event.
+//
+// Set *psub to command string in histlist (NULL if no sub)
+//     *psuffix to copy of exstr beyond substitution (can be empty)
+// Return 1 if EXSTR begins with valid substitution event
+//        0 if EXSTR does not begin with a substitution event
+//       -1 if EXSTR begins with an impossible substitution event
+//
+static int get_sub_suf (char *exstr, char **psub, char **psuffix) {
+    int status;
+    
+    // "!!"
+    if (exstr[1] == '!') {
+        
+        if (histsize >= 1) {
+            *psub =;
+        }
+    }
+}
+
+static Histlist nthlast (Histlist h, )
+{
+}
+
+// Return string of recursively concatenated 
+// texts in token list, or NULL if list is NULL
+static char* stringify (token *list) {
+    if (list == NULL)
+        return NULL;
+    else {
+        char *rest = stringify (list->next);
+        char *result = concat (list->text, rest);
+        free(rest);
+        return result;
+    }
+}
+
+// Return concatenation of s1 and s2, separated by blank.
+// If s2 NULL, return copy of s1.
+static char* concat (const char *s1, const char *s2) {
+    char *result;
+    if (s2 == NULL) {
+        result = malloc(sizeof(char) * (strlen(s1) + 1));
+        strcpy(result, s1);
+    }        
+    else {
+        result = malloc(sizeof(char) * (strlen(s1)+strlen(s2)+2));
+        strncpy(result, s1, strlen(s1));
+        result[strlen(s1)] = ' ';
+        strcpy(result+strlen(s1)+1, s2);
+    }
+    return result;
 }
 
 // Add LIST and NCMD to HIST
